6350f5edf7c261a030b0d9ef606fd75aaf104525
Denys <bakhonin1@gmail.com>
2024-04-04 11:03:59 +0200

    TypeScript home work 2

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,15 +3,29 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
-  return new Promise((resolve) => {
-    resolve(['Text', 50]);
+function getPromise(): Promise<(string | number)[]> {
+  return new Promise<(string | number)[]>((resolve) => {
+    resolve(["Text", 50]);
   });
 }
 
-getPromise()
-.then((data) => {
+getPromise().then((data) => {
   console.log(data);
 });
 
 export {};
+
+// function getPromise(): Promise<(string | number)[]> {
+//   return new Promise((resolve) => {
+//     resolve(["Text", 50]);
+//   });
+// }
+
+// getPromise().then((data) => {
+//   console.log(data);
+// });
+
+// export {};
+
+
+//У цьому завданні потрібно явно вказувати тип значення, що повертається?
